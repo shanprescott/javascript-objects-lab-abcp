5e822b289132105966c5871e946d2a81783f25c0
Shannon Prescott <shan.prescott@outlook.com>
2018-01-19 17:30:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ var recipes = {
 
 
 function updateObjectWithKeyAndValue(object, key, value){
-  return Object.assign(object, {[key]: value})
+  return Object.assign(object, {[key]: value});
 }
 
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   object[key] = value;
-  return object
+  return object;
 
 }
 
